3f10c1b65df7b078bfede7ace849722e9d78bb89
IsmaBona <ismael.bonachia@opendeusto.es>
2023-11-28 01:43:22 +0100

    tablas añadidas a la bd

--- proyectoIw/urls.py
+++ proyectoIw/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
- path('appMammaMia/', include('appMammaMia.urls')),
  path('admin/', admin.site.urls),
+ path('appMammaMia/', include('appMammaMia.urls')),
 ]
