f36392e95066b58edf009ed9046a0de410f66457
begonarp <bego.r@opendeusto.es>
2023-12-02 12:43:00 +0100

    bd

--- appMammaMia/admin.py
+++ appMammaMia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ingrediente
 
 # Register your models here.
+
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'vegetariano')
+    search_fields = ('nombre',) 
