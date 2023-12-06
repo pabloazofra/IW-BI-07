41b5160c7253855e744457b6fa9ed8a38cacf883
IsmaBona <ismael.bonachia@opendeusto.es>
2023-12-06 03:13:08 +0100

    bd

--- appMammaMia/models.py
+++ appMammaMia/models.py
@@ -67,7 +67,7 @@ class Bebida(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='bebidas/')
+    imagen = models.ImageField()
 
     def __str__(self):
         return self.nombre
