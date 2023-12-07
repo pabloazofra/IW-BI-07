be73387baede21bddd0a94ba25e6d0baca122454
IsmaBona <ismael.bonachia@opendeusto.es>
2023-12-07 02:59:47 +0100

    db

--- appMammaMia/models.py
+++ appMammaMia/models.py
@@ -28,7 +28,7 @@ class Pizza(models.Model):
     masa = models.ForeignKey(Masas, on_delete=models.CASCADE)
     ingredientes = models.ManyToManyField(Ingrediente)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='pizzas/')
+    imagen = models.ImageField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     disponible = models.BooleanField(default=True)
     calorias = models.IntegerField()
@@ -58,7 +58,7 @@ class Entrante(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='entrantes/')
+    imagen = models.ImageField()
 
     def __str__(self):
         return self.nombre
