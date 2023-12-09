75800149fc1272d5093580dbc67808ba1906915b
IsmaBona <ismael.bonachia@opendeusto.es>
2023-12-09 21:25:16 +0100

    vue

--- appMammaMia/urls.py
+++ appMammaMia/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('contacto/', views.contacto, name='contacto'),
     path('masas/', views.masas, name='masas'),
     path('masas/<int:nombre>/', views.detalles_masa, name='detalles_masa'),
+    path('javascript/', views.javascript_view, name='javascript_view'),
+
 ]
